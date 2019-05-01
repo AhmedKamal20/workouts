7f61433b21d4e4be45a01d8729f8fee210129a52
AhmedKamal20 <ahmed.kamal200@yahoo.com>
2019-05-01 04:28:55 +0200

    Refactor Workout Components

--- src/components/AddWorkOut.js
+++ src/components/AddWorkOut.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { FirebaseContext } from '../clients/Firebase';
+
 import uuidv4 from '../libs/uuidv4';
 
-import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import Checkbox from '@material-ui/core/Checkbox'
 import FormControl from '@material-ui/core/FormControl'
@@ -12,14 +12,24 @@ import InputLabel from '@material-ui/core/InputLabel'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-import { Search } from '@material-ui/icons'
+const bodyParts = [
+  'Chest',
+  'Legs',
+  'Back',
+];
+
+const initialState = {workout: {name: "", bodyParts: [], active: true}, loading: false};
 
 class AddWorkOut extends React.Component {
 
   constructor() {
     super();
-    this.state = {workout: {}};
+    this.state = initialState;
   }
 
   handleChange = (event) => {
@@ -32,69 +42,81 @@ class AddWorkOut extends React.Component {
   }
 
   handleSubmit = (event) => {
+    this.setState({loading: true});
     event.preventDefault();
     const firebase = this.context;
     firebase
       .workout(uuidv4())
-      .set(this.state.workout);
+      .set(this.state.workout).then(() => {
+        this.setState(initialState);
+      });
   }
 
   render() {
+    const { workout, loading } = this.state;
     return(
-      <Grid item xs={12}>
-        <Grid container justify="center" spacing={16}>
-          <Grid item>
-            <Paper style={{padding: 15}}>
-              <Avatar>
-                <Search />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Fill In
-              </Typography>
-              <form onSubmit={this.handleSubmit}>
-                <FormControl margin="normal" required fullWidth>
-                  <InputLabel htmlFor="email">Email Address</InputLabel>
-                  <Input
-                    id="email"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                    onChange={this.handleChange}
-                  />
-                </FormControl>
-                <FormControl margin="normal" required fullWidth>
-                  <InputLabel htmlFor="password">Password</InputLabel>
-                  <Input
-                    name="password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    onChange={this.handleChange}
-                  />
-                </FormControl>
-                <FormControlLabel
-                  control={<Checkbox
-                             name="remember"
-                             value="remember"
-                             color="primary"
-                             onChange={this.handleChange}
-                           />}
-                  label="Remember me"
+      <Grid container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+      >
+        <Grid item xs={3}>
+          <Paper style={{padding: 15}}>
+            <Typography component="h1" variant="h5">
+              WorkOut Info
+            </Typography>
+            <form onSubmit={this.handleSubmit} id="hello">
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="name">Name</InputLabel>
+                <Input
+                  id="name"
+                  name="name"
+                  value={workout.name}
+                  autoFocus
+                  onChange={this.handleChange}
                 />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="bodyParts">Body Parts</InputLabel>
+                <Select
+                  multiple
+                  name="bodyParts"
+                  value={workout.bodyParts}
+                  onChange={this.handleChange}
+                  input={<Input id="bodyParts" />}
+                  renderValue={selected => selected.join(', ')}
                 >
-                  Submit
-                </Button>
-              </form>
-            </Paper>
-          </Grid>
+                  {bodyParts.map(bodyPart => (
+                    <MenuItem key={bodyPart} value={bodyPart}>
+                      <Checkbox color="primary" checked={workout.bodyParts.indexOf(bodyPart) > -1} />
+                      <ListItemText primary={bodyPart} />
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControlLabel
+                control={<Checkbox
+                           name="active"
+                           value="active"
+                           checked={workout.active}
+                           color="primary"
+                           onChange={this.handleChange}
+                         />}
+                label="Active?"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color={loading ? "secondary" : "primary"}
+              >
+                {loading ? <CircularProgress color="inherit" size={24} /> : "Submit"}
+              </Button>
+            </form>
+          </Paper>
         </Grid>
       </Grid>
-
     )
   }
 }
--- src/components/ListWorkOuts.js
+++ src/components/ListWorkOuts.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { FirebaseContext } from '../clients/Firebase';
-import PropTypes from 'prop-types';
 
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import TableBody from '@material-ui/core/TableBody'
+import Checkbox from '@material-ui/core/Checkbox'
+import IconButton from '@material-ui/core/IconButton'
+import LinearProgress from '@material-ui/core/LinearProgress'
+
+import { Delete } from '@material-ui/icons'
 
 const styles = theme => ({
-  paper: {
-    padding: theme.spacing.unit * 2,
-    margin: 16,
-    maxWidth: 500,
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 2,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
   },
 });
 
@@ -21,7 +34,7 @@ class ListWorkOuts extends React.Component {
     super();
     this.state = {
       loading: false,
-      users: [],
+      workouts: [],
     };
   }
 
@@ -29,38 +42,85 @@ class ListWorkOuts extends React.Component {
     this.setState({ loading: true });
     const firebase = this.context;
 
-    firebase.users().on('value', snapshot => {
-      const usersObject = snapshot.val();
-      const usersList = Object.keys(usersObject).map(key => ({
-        ...usersObject[key],
-        uid: key,
-      }));
-      this.setState({
-        users: usersList,
-        loading: false,
-      });
+    firebase.workouts().on('value', snapshot => {
+      if (snapshot.exists()) {
+        const workoutsObject = snapshot.val();
+        const workoutsList = Object.keys(workoutsObject).map(key => ({
+          ...workoutsObject[key],
+          uid: key,
+        }));
+        this.setState({
+          workouts: workoutsList,
+          loading: false,
+        });
+      } else {
+        this.setState({
+          loading: false,
+          workouts: [],
+        });
+      }
     });
+  }
+
+  componentWillUnmount() {
+    const firebase = this.context;
+    firebase.workouts().off();
+  }
+
+  handleDelete = (uid) => {
+    const firebase = this.context;
+    if (uid) {
+      firebase.workout(uid).remove();
+    }
   }
 
   render() {
     const { classes } = this.props;
+    const { workouts } = this.state;
     return (
       <div>
-        <Grid container spacing={16}>
-          <Grid item xs={12}>
-            <Grid container spacing={16}>
-              {this.state.users.map(value => (
-                <Grid key={value.uid} item>
-                  <Paper className={classes.paper}>
-                    <Typography component="h1" variant="h5">
-                      {value.email}
-                    </Typography>
-                  </Paper>
-                </Grid>
+      <Grid container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+      >
+      <Grid item xs={10}>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Body Parts</TableCell>
+                <TableCell align="right">Active?</TableCell>
+                <TableCell align="right">Delete</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {workouts.map(workout => (
+                <TableRow key={workout.uid}>
+                  <TableCell component="th" scope="row">
+                    {workout.name}
+                  </TableCell>
+                  <TableCell align="right">{workout.bodyParts && workout.bodyParts.join(', ')}</TableCell>
+                  <TableCell align="right">
+                    <Checkbox disabled checked={workout.active} value="checkedE" />
+                  </TableCell>
+                  <TableCell align="right">
+                    <IconButton aria-label="Delete" hamda="ajs" onClick={() => this.handleDelete(workout.uid)}>
+                      <Delete />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
               ))}
-            </Grid>
-          </Grid>
-        </Grid>
+            </TableBody>
+          </Table>
+          {this.state.loading &&
+            <LinearProgress color="secondary" />
+          }
+        </Paper>
+      </Grid>
+      </Grid>
       </div>
     );
   }
